11fb520d065a48bb04b38191ec0ce298e8523a70
Eden Candelas <e.candelas@gmail.com>
2014-10-26 20:23:56 -0600

    se agregan dos funciones:\n listarPuertos-recibe un listado vacio y llama serial.tools.list_ports.grep() parar obtener un listado de los puertos disponibles, se regresa una lista solo con los nombres de los puertos\n seleccionarPuerto()- recibe el listado de nombres de puertos disponibles y despliega una lista en pantalla para seleccionar de entre los disponibles, regresa el index del puerto seleccionado

--- bbio-cmd.py
+++ bbio-cmd.py
@@ -1,13 +1,26 @@
 from sys import argv
 import serial
+import serial.tools.list_ports
 
-script, puerto = argv
+
 
 cmdON = []
 
 cmdOFF = []
 
 prompt = '>'
+separador = "++++++++++++++++++++++++++++++"
+pattern = '\/dev\/tty[A-Z]{3}[0-0]*'
+
+
+def listarPuertos(p):
+	iterable = serial.tools.list_ports.grep(pattern)
+#	p = serial.tools.list_ports.comports()
+#	print p
+	for i in iterable:
+		p.append(i[0])
+		print i[0]
+	return p
 
 def enviarComando(io, cmd):
 	if cmd == "on":
@@ -45,11 +58,58 @@ def terminarBoard(board):
 	else:
 		print "Board inexistente"
 
-board = iniciarBoard(puerto)
-terminarBoard(board)
-if board.isOpen():
-	print "sigue abierto"
-else:
-	print "yano esta"
+def seleccionarPuerto(p):
+	numero = 1
+	opciones = []
+	status = 1
+	print separador
+	print "Seleccione uno de los puertos diponibles"
+	
+	for i in p:
+		print "%d) %r" % (numero,i)
+		opciones.append(numero)
+		numero = numero + 1
+	
+	print opciones	
 
-print "Exit"	
+	while status:
+		seleccion = raw_input(prompt)
+		try:
+			index = opciones.index(int(seleccion))
+			status = 0
+		except ValueError:
+			print "Opcion %r desconocida" % seleccion
+			status = 1
+			print "Ingrese otra opcion"
+	
+	return index
+
+
+######### execution ##########
+
+#try:
+#	script, puerto = argv
+#except ValueError:
+#	print "No se proporciona puerto"
+#else:
+#	listarPuertos()
+
+puertosDisponibles = []
+
+puertosDisponibles = listarPuertos(puertosDisponibles)
+print "Salio el puerto, se pasa:"
+print puertosDisponibles
+for i in puertosDisponibles:
+	print i
+miIndex = seleccionarPuerto(puertosDisponibles)
+print miPuerto[miIndex]
+
+
+#board = iniciarBoard(puerto)
+#terminarBoard(board)
+#if board.isOpen():
+#	print "sigue abierto"
+#else:
+#	print "yano esta"
+
+#print "Exit"	
